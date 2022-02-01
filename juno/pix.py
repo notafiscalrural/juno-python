c19b3498a81e6fd834fb6bfd76dba0ecc77887fb
Leonardo Gregianin <leogregianin@gmail.com>
2022-02-01 16:11:08 -0400

    Created route to generate static qrcode PIX

--- juno/pix.py
+++ juno/pix.py
@@ -4,3 +4,7 @@ from juno.resources.routes import pix_routes
 
 def create_keys(dictionary):
     return handler_request.post(pix_routes.create_keys(), dictionary)
+
+
+def qrcodes_static(dictionary):
+    return handler_request.get(f"{pix_routes.get_base_url()}/qrcodes/static", dictionary)
